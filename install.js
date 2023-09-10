5c57132ed6b75fef79281ab77adad8577a50d26d
Oshi41 <sheglovaoe@gmail.com>
2023-09-10 15:21:31 +0700

    convert schedule from UTC to local

--- install.js
+++ install.js
@@ -1,3 +1,16 @@
 const cron = require('node-cron');
 const {run} = require('./main.js');
-cron.schedule('0 0 23,0-5 * * *', run); // by UTC
+
+let add = Math.floor(new Date().getTimezoneOffset() / 60);
+let to_local = (n)=>{
+    let res = n + add;
+    if (res < 0)
+        res += 24;
+    if (res > 0)
+        res %= 24;
+    return res;
+}
+let h_schedule = `${to_local(23, add)}-${to_local(5)}`; // by UTC
+let schedule = `0 0 ${h_schedule} * * *`;
+
+cron.schedule(schedule, run);
